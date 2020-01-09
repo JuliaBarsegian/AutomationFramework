90e7dd7ba7ecf3ee44343c02672cb7168c71d2ef
= <=>
2020-01-09 20:51:33 +0300

    first commit

---
+++ src/test/java/by/stormnet/web/AllTests.java
@@ -0,0 +1,60 @@
+package by.stormnet.web;
+
+import by.stormnet.automation.core.utils.IOUtils;
+import by.stormnet.web.helpers.LamodaHomeHelper;
+import by.stormnet.web.helpers.LoginHelper;
+import by.stormnet.web.helpers.OnlinerHomeHelper;
+import by.stormnet.web.helpers.OnlinerTechNewsHelper;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class AllTests {
+
+    private LamodaHomeHelper lamodaHomeHelper = new LamodaHomeHelper();
+
+    @Test
+    public void paginationTest() {
+        lamodaHomeHelper.openLamodaHomeHelper();
+        lamodaHomeHelper.searchAndCheckResult("nike");
+        boolean page = lamodaHomeHelper.openPageAndCheck("2");
+        boolean pageNext = lamodaHomeHelper.openNextPageAndCheck();
+        boolean pageBack = lamodaHomeHelper.openPrevPageAndCheck();
+
+        Assert.assertTrue(page && pageBack && pageNext);
+    }
+
+    private LoginHelper loginHelper = new LoginHelper();
+    private String username;
+    private String password;
+
+    @BeforeClass
+    public void setUp() {
+        username = IOUtils.loadGenericProperties("username", "configuration");
+        password = IOUtils.loadGenericProperties("password", "configuration");
+        loginHelper.openLoginPage();
+    }
+
+    @Test
+    public void loginTest() {
+        loginHelper.login(username, password);
+        Assert.assertTrue(loginHelper.checkPageWithEmailsOpened(), "User is not logged in successfully!");
+    }
+
+    private OnlinerHomeHelper onlinerHomeHelper = new OnlinerHomeHelper();
+    private OnlinerTechNewsHelper onlinerTechNewsHelper = new OnlinerTechNewsHelper();
+
+    @Test
+    public void onlinerTest() {
+        onlinerHomeHelper.openOnlinerHomePageAndOpenNews();
+        Assert.assertTrue(onlinerTechNewsHelper.openAnyNewsAndCheck(), "news is not opened!");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        onlinerHomeHelper.quite();
+        loginHelper.quite();
+        lamodaHomeHelper.quite();
+    }
+}
